7ab32ba6c7a4feb42773ab139489e34c1aecd265
Leo Zdjelarevic <116094883+leozdjela@users.noreply.github.com>
2023-05-10 09:11:22 +0200

    Update script.js

--- java.js
+++ java.js
@@ -70,3 +70,13 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// hamburger
+function myFunction() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
